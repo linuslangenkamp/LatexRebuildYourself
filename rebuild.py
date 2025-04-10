cb1bfa9833133c114dd66684913a6db5242a0c13
linuslangenkamp <linus.langenkamp@fh-bielefeld.de>
2025-04-10 17:27:37 +0200

    add makefile support

--- rebuild.py
+++ rebuild.py
@@ -35,10 +35,29 @@ def run_pdflatex(latex_file, command):
                                stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     # Simulate pressing Enter for Errors
-    stdout, stderr = process.communicate(input=b"\n" * 50)
+    stdout, stderr = process.communicate(input=b"\n" * 500)
+    try:
+        print(stdout.decode())
+        print(stderr.decode())
+    except:
+        pass
 
-    print(stdout.decode())
-    print(stderr.decode())
+def run_make(latex_file):
+    latex_dir = os.path.dirname(os.path.abspath(latex_file))
+    os.chdir(latex_dir)
+
+    # Run make command (no need to manually invoke bibtex or latexmk)
+    process = subprocess.Popen(['make'],
+                               stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # Simulate pressing Enter for Errors
+    stdout, stderr = process.communicate(input=b"\n" * 500)
+
+    try:
+        print(stdout.decode())
+        print(stderr.decode())
+    except:
+        pass
 
 def run_shell_script(latex_file, script):
     latex_dir = os.path.dirname(os.path.abspath(latex_file))
@@ -49,12 +68,16 @@ def run_shell_script(latex_file, script):
                                stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
     # Simulate pressing Enter for Errors
-    stdout, stderr = process.communicate(input=b"\n" * 50)
+    stdout, stderr = process.communicate(input=b"\n" * 500)
 
-    print(stdout.decode())
-    print(stderr.decode())
+    try:
+        print(stdout.decode())
+        print(stderr.decode())
+    except:
+        pass
 
-def check_and_compile(latex_file, timestamp_file, command, script):
+
+def check_and_compile(latex_file, timestamp_file, command, script, make):
     if os.path.exists(timestamp_file):
         with open(timestamp_file, "r") as f:
             last_mod_time = float(f.read())
@@ -64,10 +87,13 @@ def check_and_compile(latex_file, timestamp_file, command, script):
     current_mod_time = get_modification_time(latex_file)
 
     if current_mod_time > last_mod_time:
-        print(f"{latex_file} has been modified. Running {command or script}...")
+        action = command or script or ("make" if make else None)
+        print(f"{latex_file} has been modified. Running {action}...")
         
         if script:
             run_shell_script(latex_file, script)
+        elif make:
+            run_make(latex_file)
         else:
             run_pdflatex(latex_file, command)
 
@@ -82,12 +108,13 @@ def main():
     parser.add_argument("-latex", choices=["pdflatex", "lualatex", "xelatex", "tectonic"],
                         help="The LaTeX engine to use for compilation (pdflatex, lualatex, xelatex, tectonic).")
     parser.add_argument("-shell", help="Path to the shell script for compilation.")
+    parser.add_argument("-make", action="store_true", help="Trigger make for compilation.")
 
     args = parser.parse_args()
     timestamp_file = ".build_timestamp.txt"
 
     while True:
-        check_and_compile(args.latex_file, timestamp_file, args.latex, args.shell)
+        check_and_compile(args.latex_file, timestamp_file, args.latex, args.shell, args.make)
         time.sleep(1)
 
 if __name__ == "__main__":
